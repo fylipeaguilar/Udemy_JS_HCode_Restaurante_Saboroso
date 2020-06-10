0519896505b83c60b2ec93672c9b6be384248352
Fylipe Aguilar <fylipears@gmail.com>
2020-06-10 10:24:57 -0300

    first commit

---
+++ restaurante/inc/db.js
@@ -0,0 +1,17 @@
+// Tutorial de uso no "msql2"
+// https://www.npmjs.com/package/mysql2
+
+// Importando o msql2
+const mysql = require('mysql2');
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'user',
+  database: 'saboroso',
+  password: 'password'
+});
+
+// Fazendo o exporte para quem fez a chamada
+// passando o nosso objeto de conexão ("connection" que é o nome da const)
+module.exports = connection;
---
+++ restaurante/routes/users.js
@@ -0,0 +1,29 @@
+// Fazendo o require da nossa conexão com o banco de dados 
+// Assim poderemos usar a variável com nas rotas
+var conn = require('./../inc/db')
+
+// Fazendo o require do express
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  
+  // Modelo de uso do select (https://www.npmjs.com/package/mysql2)
+  conn.query|("SELECT * FROM tb_users ORDER BY name", (err, results) => {
+
+    if(err){
+
+      res.send(err)
+
+    } else {
+
+      res.send(results)
+
+    }
+
+  })
+
+});
+
+module.exports = router;
